0a2c92914a00fb7f0add36a1ed529a3224670826
verdipratama <verdipratama@icloud.com>
2020-11-16 01:50:41 +0700

    fix server

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === 'PRODUCTION_MODE') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')),
+    res.sendFile(path.resolve(__dirname, 'bin', 'build', 'index.html')),
   );
 }
 
